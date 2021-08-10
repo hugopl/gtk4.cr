370e44a38adda8011f3d1fff926879c7c41d025e
Hugo Parente Lima <hugo.pl@gmail.com>
2021-08-10 20:18:25 -0300

    Add GTK::Builder example.

---
+++ examples/builder.cr
@@ -0,0 +1,37 @@
+require "../src/gtk4"
+
+UI =<<-EOT
+<?xml version="1.0" encoding="UTF-8"?>
+<interface>
+  <requires lib="gtk" version="4.0"/>
+  <object class="GtkBox" id="root">
+    <property name="can-focus">0</property>
+    <property name="orientation">vertical</property>
+    <child>
+      <object class="GtkLabel">
+        <property name="can-focus">0</property>
+        <property name="label" translatable="yes">Hope someone add GTK4
+support to glade soon</property>
+        <property name="justify">right</property>
+      </object>
+    </child>
+  </object>
+</interface>
+EOT
+
+
+def activate(app : Gtk::Application)
+  window = Gtk::ApplicationWindow.new(app)
+  window.title = "Gtk Builder"
+  window.set_default_size(200, 200)
+
+  builder = Gtk::Builder.from_string(UI, UI.bytesize.to_i64)
+  root = Gtk::Widget.cast(builder.get_object("root"))
+
+  window.child = root
+  window.present
+end
+
+app = Gtk::Application.new("hello.example.com", Gio::ApplicationFlags::None)
+app.activate_signal.connect(->activate(Gtk::Application))
+exit(app.run(ARGV))
