f31dd841939aaebdf8ab2a9c1925dec27a59c3da
Hugo Parente Lima <hugo.pl@gmail.com>
2021-08-21 17:17:59 -0300

    Add Gtk::Entry example.

---
+++ examples/entry.cr
@@ -0,0 +1,32 @@
+require "../src/gtk4"
+
+def activate(app : Gtk::Application)
+  window = Gtk::ApplicationWindow.new(app)
+  window.title = "Entry"
+
+  box = Gtk::Box.new(:vertical, 0)
+  label = Gtk::Label.new(use_markup: true)
+  entry = Gtk::Entry.new
+
+  entry.notify_signal["text"].connect do
+    label.text = entry.text
+  end
+
+  entry.activate_signal.connect do
+    label.markup = "<b>#{label.text}</b>"
+    GLib.timeout(1.seconds) do
+      label.markup = label.text
+      false
+    end
+  end
+
+  box.prepend(label)
+  box.append(entry)
+  window.child = box
+
+  window.present
+end
+
+app = Gtk::Application.new("hello.example.com", Gio::ApplicationFlags::None)
+app.activate_signal.connect(->activate(Gtk::Application))
+exit(app.run(ARGV))
