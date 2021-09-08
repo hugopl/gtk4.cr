00ba6c999d6445cb339658d99759c124a0a20e6a
Hugo Parente Lima <hugo.pl@gmail.com>
2021-09-08 01:28:19 -0300

    Make smoke tests fail if some example failed to compile.

--- spec/smoke_test_spec.cr
+++ spec/smoke_test_spec.cr
@@ -8,7 +8,8 @@ describe "GTK4 Bindings smoke tests" do
   {% for example in `ls -1 examples/*.cr`.strip.split("\n") %}
   it "compiles #{{{ example }}}" do
     example_bin = File.basename({{ example }}, ".cr")
-    `crystal build #{{{ example }}} -o ./bin/#{example_bin}`
+    `crystal build #{{{example}}} -o ./bin/#{example_bin}`
+    $?.success?.should eq(true)
   end
   {% end %}
 end
