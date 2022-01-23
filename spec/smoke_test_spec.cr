c618d7539e3f3097cc13b203a084950b89c45d43
Hugo Parente Lima <hugo.pl@gmail.com>
2022-01-23 16:19:01 -0300

    Some love to docs.

--- spec/smoke_test_spec.cr
+++ spec/smoke_test_spec.cr
@@ -4,7 +4,7 @@ describe "GTK4 Bindings smoke tests" do
   {% for example in `ls -1 examples/*.cr`.strip.split("\n") %}
   it "compiles #{{{ example }}}" do
     example_bin = File.basename({{ example }}, ".cr")
-    `crystal build #{{{example}}} -o ./bin/#{example_bin}`
+    `crystal build #{{{example}}} -o ./bin/test_example`
     $?.success?.should eq(true)
   end
   {% end %}
