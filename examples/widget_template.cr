c93354e5c181890cca686d258b180fcd63bb88c4
Hugo Parente Lima <hugo.pl@gmail.com>
2022-04-17 15:57:51 -0300

    Add support for XML widget templates! 🎉
    😎

---
+++ examples/widget_template.cr
@@ -0,0 +1,15 @@
+require "../src/gtk4"
+
+@[Gtk::UiTemplate(file: "#{__DIR__}/widget_template.ui")]
+class ExampleAppWindow < Gtk::ApplicationWindow
+  include Gtk::WidgetTemplate
+end
+
+app = Gtk::Application.new("hello.example.com", Gio::ApplicationFlags::None)
+app.activate_signal.connect do
+  window = ExampleAppWindow.new
+  window.application = app
+  window.present
+end
+
+exit(app.run)
---
+++ src/bindings/gtk/widget_template.cr
@@ -0,0 +1,48 @@
+lib LibGLib
+  fun g_bytes_new_static(data : Void*, size : LibC::SizeT) : Void*
+end
+
+module Gtk
+  annotation UiTemplate
+  end
+
+  # Module used to implement widgets using XML templates.
+  #
+  # The example bellow creates a window with the widgets defined by the
+  # XML file _widget_template.ui_
+  #
+  # ```Crystal
+  # @[Gtk::UiTemplate(file: "#{__DIR__}/widget_template.ui")]
+  # class ExampleAppWindow < Gtk::ApplicationWindow
+  #   include Gtk::WidgetTemplate
+  # end
+  #
+  # app = Gtk::Application.new("hello.example.com", Gio::ApplicationFlags::None)
+  # app.activate_signal.connect do
+  #   window = ExampleAppWindow.new
+  #   window.application = app
+  #   window.present
+  # end
+  #
+  # exit(app.run)
+  # ```
+  module WidgetTemplate
+    macro included
+      def self._class_init(klass : Pointer(LibGObject::TypeClass), user_data : Pointer(Void)) : Nil
+        previous_def
+
+        {% unless @type.annotation(Gtk::UiTemplate) %}
+        {% raise "You must annotate #{@type} with Gtk::UiTemplate." %}
+        {% end %}
+        data = {{ read_file(@type.annotation(Gtk::UiTemplate)[:file]) }}
+        gbytes = LibGLib.g_bytes_new_static(data, data.bytesize)
+        LibGtk.gtk_widget_class_set_template(klass, gbytes)
+      end
+
+      def self._instance_init(instance : Pointer(LibGObject::TypeInstance), type : Pointer(LibGObject::TypeClass)) : Nil
+        previous_def
+        LibGtk.gtk_widget_init_template(instance)
+      end
+    end
+  end
+end
