526d165c4b1c744a2d1da8961bfe25e1eb50bc1a
Hugo Parente Lima <hugo.pl@gmail.com>
2022-04-23 00:28:33 -0300

    Fix smoke tests after `c4f6975a402bf025677b53dba125330f9a40009f`.

--- spec/smoke_test_spec.cr
+++ spec/smoke_test_spec.cr
@@ -3,9 +3,11 @@ require "./spec_helper"
 describe "GTK4 Bindings smoke tests" do
   {% for example in `ls -1 examples/*.cr`.strip.split("\n") %}
   it "compiles #{{{ example }}}" do
-    example_bin = File.basename({{ example }}, ".cr")
-    `crystal build #{{{example}}} -o ./bin/#{example_bin}`
-    $?.success?.should eq(true)
+    example_name = File.basename({{ example }}, ".cr")
+    Dir.cd("examples") do
+      `crystal build #{example_name}.cr -o ../bin/#{example_name}`
+      $?.success?.should eq(true)
+    end
   end
   {% end %}
 end
