587d29c5e0eff08cbae9de1d241b5d8ce57567cd
Hugo Parente Lima <hugo.pl@gmail.com>
2022-06-04 17:36:03 -0300

    Fix widget test name.

--- spec/widget_spec.cr
+++ spec/widget_spec.cr
@@ -11,7 +11,7 @@ class BadWidget < Gtk::Widget
 end
 
 describe Gtk::Widget do
-  it "raises Gtk::BuilderError::InvalidId when using invalid ids in template widgets" do
+  it "abort when using invalid ids in template widgets" do
     expect_raises(AbortCalled, "XML element with id: 'typo' not found for BadWidget template.") { BadWidget.new }
   end
 end
