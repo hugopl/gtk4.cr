acaa26f03ce432ffa98cfeed6a73cc3fae2efcaf
Hugo Parente Lima <hugo.pl@gmail.com>
2022-06-28 21:04:12 -0300

    Oops.

--- src/bindings/pango/layout.cr
+++ src/bindings/pango/layout.cr
@@ -1,11 +1,11 @@
 module Pango
   class Layout < GObject::Object
     def set_text(text : ::String) : Nil
-      LibPango.pango_layout_set_text(to_unsafe, text, text.length)
+      LibPango.pango_layout_set_text(to_unsafe, text, text.bytesize)
     end
 
     def set_text(bytes : Bytes)
-      LibPango.pango_layout_set_text(to_unsafe, bytes, bytes.length)
+      LibPango.pango_layout_set_text(to_unsafe, bytes, bytes.size)
     end
 
     def set_text(bytes : Bytes, length : Int32)
