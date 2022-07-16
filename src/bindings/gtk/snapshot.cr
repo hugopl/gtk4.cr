027e5196e896ebf55aa4b469c05ac874ea29c136
Hugo Parente Lima <hugo.pl@gmail.com>
2022-07-16 17:00:45 -0300

    Add some more helpers for Gtk::Snapshot.

--- src/bindings/gtk/snapshot.cr
+++ src/bindings/gtk/snapshot.cr
@@ -6,6 +6,7 @@ module Gtk
       LibGtk.gtk_snapshot_translate(to_unsafe, pointerof(point))
     end
 
+    # Same as `Gtk::Snapshot#append_color`, but receives a bunch of Floats32.
     def append_color(color : Gdk::RGBA, x : Float32, y : Float32, width : Float32, height : Float32) : Nil
       bounds = uninitialized LibGraphene::Rect
       bounds.origin.x = x
@@ -13,6 +14,33 @@ module Gtk
       bounds.size.width = width
       bounds.size.height = height
       LibGtk.gtk_snapshot_append_color(to_unsafe, color, pointerof(bounds))
+    end
+
+    # Same as `Gtk::Snapshot#push_repeat`, but receives a bunch of Floats32.
+    def push_repeat(bounds_x : Float32, bounds_y : Float32, bounds_width : Float32, bounds_height : Float32,
+                    child_bounds_x : Float32, child_bounds_y : Float32,
+                    child_bounds_width : Float32, child_bounds_height : Float32) : Nil
+      bounds = uninitialized LibGraphene::Rect
+      bounds.origin.x = bounds_x
+      bounds.origin.y = bounds_y
+      bounds.size.width = bounds_width
+      bounds.size.height = bounds_height
+      child_bounds = uninitialized LibGraphene::Rect
+      child_bounds.origin.x = child_bounds_x
+      child_bounds.origin.y = child_bounds_y
+      child_bounds.size.width = child_bounds_width
+      child_bounds.size.height = child_bounds_height
+
+      LibGtk.gtk_snapshot_push_repeat(to_unsafe, pointerof(bounds), pointerof(child_bounds))
+    end
+
+    # Same as `Gtk::Snapshot#save`, but receives a block and calls `Gtk::Snapshot#restore`
+    # automatically
+    def save
+      LibGtk.gtk_snapshot_save(to_unsafe)
+      yield
+    ensure
+      LibGtk.gtk_snapshot_restore(to_unsafe)
     end
   end
 end
