1af7fac50b06351682fec37ca4d6881b42710ff8
Hugo Parente Lima <hugo.pl@gmail.com>
2022-10-18 18:52:04 -0300

    Fix tests crash.
    Test code were calling `LibGLib.g_log_set_writer_func` multiple times, for some reason this
    worker in previous version of GTK4, but crashed now on 4.8.1 on ArchLinux.

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,8 +1,6 @@
 require "spec"
 require "../src/gtk4"
 
-LibGtk.gtk_init
-
 # If any code calls ::abort, this exception will be called instead.
 class AbortCalled < RuntimeError
 end
@@ -12,13 +10,29 @@ def abort(message)
   raise AbortCalled.new("ABORT: #{message}")
 end
 
-private def do_nothing_log_handler(a : Int32, b : Pointer(Void), c : UInt64, d : Pointer(Void)) : Int32
-  1
+private class WarnSupressor
+  class_property? supress = false
+  def self.supress
+    @@supress = true
+    yield
+  ensure
+    @@supress = false
+  end
 end
 
+private def do_nothing_log_handler(a : Int32, b : Pointer(LibGLib::LogField), c : UInt64, d : Pointer(Void)) : UInt32
+  if WarnSupressor.supress?
+    GLib::LogWriterOutput::Handled.value
+  else
+    LibGLib.g_log_writer_default(a, b, c, d)
+  end
+end
+
+LibGtk.gtk_init
+LibGLib.g_log_set_writer_func(->do_nothing_log_handler(Int32, Pointer(LibGLib::LogField), UInt64, Pointer(Void)).pointer, nil, nil)
+
 def supress_warnings
-  LibGLib.g_log_set_writer_func(->do_nothing_log_handler(Int32, Pointer(Void), UInt64, Pointer(Void)).pointer, nil, nil)
-  yield
-ensure
-  LibGLib.g_log_set_writer_func((->LibGLib.g_log_writer_default).pointer, nil, nil)
+  WarnSupressor.supress do
+    yield
+  end
 end
