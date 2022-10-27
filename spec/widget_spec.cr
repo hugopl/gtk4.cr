ef2eb03b38b25ea2780e83cf50af3a6807ac6293
Hugo Parente Lima <hugo.pl@gmail.com>
2022-10-27 00:25:09 -0300

    Add `Gtk::Widget#children` helper method.
    New method returns an `Iterator(Gtk::Widget)` and is implemented
    using `Gtk::Widget#first_child` and `Gtk::Widget#next_sibling`.

--- spec/widget_spec.cr
+++ spec/widget_spec.cr
@@ -14,4 +14,16 @@ describe Gtk::Widget do
   it "abort when using invalid ids in template widgets" do
     expect_raises(AbortCalled, "XML element with id: 'typo' not found for BadWidget template.") { BadWidget.new }
   end
+
+  it "has #children method" do
+    widget = Gtk::Box.new
+    widget.children.should be_a(Iterator(Gtk::Widget))
+    widget.children.size.should eq(0)
+
+    child1 = Gtk::Label.new
+    widget.append(child1)
+    child2 = Gtk::Label.new
+    widget.append(child2)
+    widget.children.to_a.should eq([child1, child2])
+  end
 end
--- src/bindings/gtk/widget.cr
+++ src/bindings/gtk/widget.cr
@@ -1,4 +1,22 @@
 module Gtk
+  private struct WidgetChildrenIterator
+    include Iterator(Widget)
+
+    def initialize(widget : Widget)
+      @child = widget.first_child
+    end
+
+    def next
+      child = @child
+      if child.nil?
+        stop
+      else
+        @child = child.next_sibling
+        child
+      end
+    end
+  end
+
   class Widget < GObject::InitiallyUnowned
     # Convenient function, same as `template_child(self.class.g_type, name)`.
     def template_child(name : String) : GObject::Object
@@ -10,6 +28,18 @@ module Gtk
 
       abort("XML element with id: '#{name}' not found for #{self.class} template.") if ptr.null?
       GObject::Object.new(ptr, GICrystal::Transfer::None)
+    end
+
+    # Returns an `Iterator` over widget children. Call `.to_a` on it if you need to allocate an
+    # array with all children.
+    #
+    # ```
+    # widget.children.with_index do |child, i|
+    #   puts "Hi, I'm child #{i}, #{child}"
+    # end
+    # ```
+    def children : Iterator(Widget)
+      WidgetChildrenIterator.new(self)
     end
   end
 end
