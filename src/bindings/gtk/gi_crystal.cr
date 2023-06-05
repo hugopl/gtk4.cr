ecdab75a81b579920d8b8c33bd37a064f76721ab
Hugo Parente Lima <hugo.pl@gmail.com>
2023-06-05 13:00:34 -0300

    Do nothing when finalizing a `Gtk::Window`.
    At finalize time the window GObject was already deleted by GTK itself.

--- src/bindings/gtk/gi_crystal.cr
+++ src/bindings/gtk/gi_crystal.cr
@@ -1,6 +1,11 @@
 module GICrystal
-  # Finalize *object*.
+  # Finalize a `Gtk::Expression` *object*.
   def finalize_instance(object : Gtk::Expression)
     LibGtk.gtk_expression_unref(object)
+  end
+
+  # Finalize a `Gtk::Window` *object*.
+  def finalize_instance(object : Gtk::Window)
+    # When we get here GTK already destroyed the object if it's a Window
   end
 end
